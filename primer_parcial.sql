17b96932e8cb22d878c38560d7fde9e02001a39c
Fverdun <fverduntorres@gmail.com>
2024-04-17 22:42:16 -0400

    Se ajusta el script para el funcionamiento correcto

--- primer_parcial.sql
+++ primer_parcial.sql
@@ -1,48 +1,45 @@
-CREATE DATABASE primer_parcial;
-USE primer_parcial;
+CREATE TABLE cliente(
+    id_cliente numeric(10),
+    id_banco numeric(10),
+    nombre varchar(50),
+    apellido varchar(50),
+    documento varchar(20),
+    direccion varchar(50),
+    mail varchar(50),
+    celular numeric(10),
+    estado varchar(1),
+    primary key(id_cliente)
+);
 
-CREATE TABLE CLIENTE(
-    id_cliente numeric(10),
-    id_banco numeric(10),
-    nombre varchar(50),
-    apellido varchar(50),
-    documento varchar(20),
-    direccion varchar(50),
-    mail varchar(50),
-    celular numeric(10),
-    estado varchar(1),
-    primary key(id_cliente)
+CREATE TABLE factura(
+    id_factura numeric(10),
+    id_cliente numeric(10),
+    nro_factura varchar(14),
+    fecha_hora varchar(18),
+    total numeric(18),
+    total_iva5 numeric(18),
+    total_iva10 numeric(18),
+    total_iva numeric(18),
+    total_letras varchar(100),
+    sucursal varchar(50),
+    primary key(id_factura),
+    foreign key(id_cliente) references cliente(id_cliente)
 );
- 
-CREATE TABLE FACTURA(
-    id_factura numeric(10),
-    id_cliente numeric(10),
-    nro_factura varchar(14),
-    fecha_hora varchar(18),
-    total numeric(18),
-    total_iva5 numeric(18),
-    total_iva10 numeric(18),
-    total_iva numeric(18),
-    total_letras varchar(100),
-    sucursal varchar(50),
-    primary key(id_factura),
-    foreign key(id_cliente) references cliente(id_cliente)
-);
- 
-CREATE SEQUENCE public.id_cliente
-    INCREMENT 1
-    START 1
-    MINVALUE 1
-    OWNED BY cliente.id_cliente;
- 
-ALTER SEQUENCE public.id_cliente
-    OWNER TO postgres;
-   
-CREATE SEQUENCE public.id_factura
-    INCREMENT 1
-    START 1
-    MINVALUE 1
-    OWNED BY factura.id_factura;
- 
-ALTER SEQUENCE public.id_factura
-    OWNER TO postgres;
+
+CREATE SEQUENCE public.seq_id_cliente
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    OWNED BY cliente.id_cliente;
+
+ALTER SEQUENCE public.seq_id_cliente
+    OWNER TO postgres;
+
+CREATE SEQUENCE public.seq_id_factura
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    OWNED BY factura.id_factura;
+
+ALTER SEQUENCE public.seq_id_factura
+    OWNER TO postgres;
